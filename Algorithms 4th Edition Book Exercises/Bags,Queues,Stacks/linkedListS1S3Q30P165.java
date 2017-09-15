3dee77d499124449a151cf992d0ef6af69e3286b
danietzio <dani.nasiri@gmail.com>
2017-09-15 10:12:55 +0430

    Adding Iterative version of the Q1.3.30

--- Algorithms 4th Edition Book Exercises/Bags,Queues,Stacks/linkedListS1S3Q30P165.java
+++ Algorithms 4th Edition Book Exercises/Bags,Queues,Stacks/linkedListS1S3Q30P165.java
@@ -16,6 +16,33 @@ class linkListS1S3Q30<Item> extends linkListS1S3Q28<Item> {
 		first = p0;
 		return p0;
 	}
+	
+	Node recursive_reverse_list(Node node) {
+		
+		// If we get to the end of the linked list, we should return node
+		if( node.next == null ) {
+			first = node;
+			return node;
+		}
+		
+		// Holding track of the node.next
+		Node temp = node.next;
+		
+		// We should make "node.next" null because if we don't do this
+		// we'll have node that points to next node after last execution of the
+		// recursive function while because that node is the last node in new
+		// created linked list, it should point to null value
+		node.next = null;
+		
+		// Recalling recursive function
+		// after that next line recursive function call, executed
+		// we'll have pointer to next node
+		Node a = recursive_reverse_list(temp);
+		
+		// Making next node to point to this node
+		a.next = node;
+		return node;
+	}
 }
 
 public class linkedListS1S3Q30P165 {
@@ -30,7 +57,7 @@ public class linkedListS1S3Q30P165 {
 		
 		StdOut.println(list.toString());
 		
-		list.iterative_reverse_list(list.first);
+		list.recursive_reverse_list(list.first);
 		
 		StdOut.println(list.toString());
 	}
